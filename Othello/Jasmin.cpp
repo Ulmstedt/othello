1255460b80aeb2e483a68a794002f3b565474ab5
Ulmstedt <matte___93@hotmail.com>
2018-01-13 16:32:27 +0100

    Add special edge checks

--- Othello/Jasmin.cpp
+++ Othello/Jasmin.cpp
@@ -9,21 +9,22 @@ using namespace std;
 void Jasmin::init_pos_weights()
 {
 	// Corners
-	pos_weight[0][0] = pos_weight[0][7] = pos_weight[7][0] = pos_weight[7][7] = V_CORNER;
+	//pos_weight[0][0] = pos_weight[0][7] = pos_weight[7][0] = pos_weight[7][7] = V_CORNER;
 	// Positions hor and ver adjacent to corners
-	pos_weight[1][0] = pos_weight[0][1] = pos_weight[6][0] = pos_weight[0][6] =
-		pos_weight[7][1] = pos_weight[1][7] = pos_weight[6][7] = pos_weight[7][7] = V_ADJCORNER;
+	//pos_weight[1][0] = pos_weight[0][1] = pos_weight[6][0] = pos_weight[0][6] =
+	//	pos_weight[7][1] = pos_weight[1][7] = pos_weight[6][7] = pos_weight[7][7] = V_ADJCORNER;
 	// Positions diagonally adjacent to corners
-	pos_weight[1][1] = pos_weight[6][1] = pos_weight[1][6] = pos_weight[6][6] = V_DADJCORNER;
+	//pos_weight[1][1] = pos_weight[6][1] = pos_weight[1][6] = pos_weight[6][6] = V_DADJCORNER;
 	// Edges
-	pos_weight[0][2] = pos_weight[2][0] = pos_weight[5][0] = pos_weight[0][5] =
-		pos_weight[2][7] = pos_weight[7][2] = pos_weight[7][5] = pos_weight[5][7] = V_EDGE;
+	//pos_weight[0][2] = pos_weight[2][0] = pos_weight[5][0] = pos_weight[0][5] =
+	//	pos_weight[2][7] = pos_weight[7][2] = pos_weight[7][5] = pos_weight[5][7] = V_EDGE;
 	// Mid edges
-	pos_weight[3][0] = pos_weight[4][0] = pos_weight[0][3] = pos_weight[0][4] =
-		pos_weight[3][7] = pos_weight[4][7] = pos_weight[7][3] = pos_weight[7][4] = V_MIDEDGE;
+	//pos_weight[3][0] = pos_weight[4][0] = pos_weight[0][3] = pos_weight[0][4] =
+	//	pos_weight[3][7] = pos_weight[4][7] = pos_weight[7][3] = pos_weight[7][4] = V_MIDEDGE;
 
-	pos_weight[2][1] = pos_weight[1][2] = pos_weight[1][5] = pos_weight[2][6] =
-		pos_weight[5][1] = pos_weight[6][2] = pos_weight[5][6] = pos_weight[6][5] = 0;
+	//pos_weight[2][1] = pos_weight[1][2] = pos_weight[1][5] = pos_weight[2][6] =
+	//	pos_weight[5][1] = pos_weight[6][2] = pos_weight[5][6] = pos_weight[6][5] = 0;
+
 
 }
 
@@ -50,9 +51,75 @@ void Jasmin::compute_max_values(Board_state state, vector<Position> legal_moves)
 	}
 }
 
+bool Jasmin::on_edge(Position pos) const
+{
+	if (pos.x == 0 || pos.x == 7 || pos.y == 0 || pos.y == 7)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void Jasmin::compute_edge_values(Board_state state, vector<Position> edge_moves)
+{
+	for (Position p : edge_moves)
+	{
+		if (p.y == 0 || p.y == 7) // Top and bottom edge
+		{
+			// ### Check if opponent can take it back immediately
+			Position leftpos = { p.x - 1, p.y };
+			Position rightpos = { p.x + 1, p.y };
+			vector<Position> opp_leftflips = BoardUtil::get_flips(state, leftpos, opponent_id); // Flips if opponent places in leftpos
+			vector<Position> opp_rightflips = BoardUtil::get_flips(state, rightpos, opponent_id); // Flips if opponent places in rightpos
+			if (opp_leftflips.size() > 0 || opp_rightflips.size() > 0)
+			{
+				// This is bad, reduce score for that position
+				value_grid[p.x][p.y] -= P_EDGERETAKE;
+			}
+
+			// ### Check for deadlocks (where there is 1 square free between two own ones)
+			leftpos = { p.x - 2, p.y };
+			rightpos = { p.x + 2, p.y };
+			if ((BoardUtil::inside_board(leftpos) && state.board[leftpos.x][leftpos.y] == player_id && state.board[p.x - 1][p.y] == EMPTY)
+				|| (BoardUtil::inside_board(rightpos) && state.board[rightpos.x][rightpos.y] == player_id && state.board[p.x + 1][p.y] == EMPTY))
+			{
+				// Bad, reduce score
+				value_grid[p.x][p.y] -= P_DEADLOCK;
+			}
+		}
+		else if (p.x == 0 || p.x == 7) // Left and right edge
+		{
+			// ### Check if opponent can take it back immediately
+			Position toppos = { p.x, p.y - 1 };
+			Position botpos = { p.x, p.y + 1 };
+			vector<Position> opp_topflips = BoardUtil::get_flips(state, toppos, opponent_id); // Flips if opponent places in leftpos
+			vector<Position> opp_botflips = BoardUtil::get_flips(state, botpos, opponent_id); // Flips if opponent places in rightpos
+			if (opp_topflips.size() > 0 || opp_botflips.size() > 0)
+			{
+				// This is bad, reduce score for that position
+				value_grid[p.x][p.y] -= P_EDGERETAKE;
+			}
+
+			// ### Check for deadlocks (where there is 1 square free between two own ones)
+			toppos = { p.x, p.y - 2 };
+			botpos = { p.x, p.y + 2 };
+			if ((BoardUtil::inside_board(toppos) && state.board[toppos.x][toppos.y] == player_id && state.board[p.x][p.y - 1] == EMPTY)
+				|| (BoardUtil::inside_board(botpos) && state.board[botpos.x][botpos.y] == player_id && state.board[p.x][p.y + 1] == EMPTY))
+			{
+				// Bad, reduce score
+				value_grid[p.x][p.y] -= P_DEADLOCK;
+			}
+		}
+	}
+
+}
+
 Position Jasmin::get_best_move(vector<Position> legal_moves) const
 {
-	Position move;
+	Position move = {0, 0};
 	float top_score = -1000;
 	for (Position p : legal_moves)
 	{
@@ -69,7 +136,8 @@ Position Jasmin::get_best_move(vector<Position> legal_moves) const
 Jasmin::Jasmin(int id)
 {
 	player_id = id;
-	init_pos_weights();
+	opponent_id = (id == PLAYER1 ? PLAYER2 : PLAYER1);
+	//init_pos_weights();
 	ai_state = MINIMIZE;
 }
 
@@ -92,6 +160,7 @@ Position Jasmin::play(Board_state state)
 
 	vector<Position> legal_moves = BoardUtil::get_legal_moves(state, player_id);
 
+	// Compute start min/max values
 	if (ai_state == MINIMIZE)
 	{
 		compute_min_values(state, legal_moves);
@@ -100,5 +169,19 @@ Position Jasmin::play(Board_state state)
 	{
 		compute_max_values(state, legal_moves);
 	}
+
+	// Compute edge values
+	vector<Position> edge_moves;
+	for (Position p : legal_moves)
+	{
+		if (on_edge(p))
+		{
+			edge_moves.push_back(p);
+		}
+	}
+	compute_edge_values(state, edge_moves);
+
+
+
 	return get_best_move(legal_moves);
 }
--- Othello/Jasmin.h
+++ Othello/Jasmin.h
@@ -6,6 +6,7 @@
 #define MINIMIZE 0
 #define MAXIMIZE 1
 
+// Positional values
 #define V_CORNER 15
 #define V_ADJCORNER 1
 #define V_DADJCORNER -10
@@ -13,18 +14,35 @@
 #define V_MIDEDGE 3
 #define V_TEMPNAME 0
 
+// Situational penalties
+#define P_DEADLOCK 25
+#define P_EDGERETAKE 25
+
 using namespace std;
 
 class Jasmin : public IPlayer
 {
+	int opponent_id;
 	int ai_state;
-	float pos_weight[8][8] = { 0 };
+	float pos_weight[8][8] =
+	{
+		{ V_CORNER, 1, 4, 3, 3, 4, 1, V_CORNER },
+		{ 1, -10, 0, -1, -1, 0, -10, 1 },
+		{ 4, 0, 2, 1, 1, 2, 0, 4 },
+		{ 3, -1, 1, 1, 1, 1, -1, 3 },
+		{ 3, -1, 1, 1, 1, 1, -1, 3 },
+		{ 4, 0, 2, 1, 1, 2, 0, 4 },
+		{ 1, -10, 0, -1, -1, 0, -10, 1 },
+		{ V_CORNER, 1, 4, 3, 3, 4, 1, V_CORNER }
+	};
 	float value_grid[8][8];
 
 	void init_pos_weights();
 	void reset_value_grid();
 	void compute_min_values(Board_state state, vector<Position> legal_moves);
 	void compute_max_values(Board_state state, vector<Position> legal_moves);
+	bool on_edge(Position pos) const; // Returns true if pos is an edge position
+	void compute_edge_values(Board_state state, vector<Position> edge_moves);
 	Position get_best_move(vector<Position> legal_moves) const;
 public:
 	Jasmin(int id);
